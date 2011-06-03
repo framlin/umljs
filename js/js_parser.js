344a35555d0ae4e457ca8bccfb156920a5cbdece
Wolfgang Egger <w.egger@framlin.de>
2011-06-03 22:43:15 +0200

    use stack

--- js/js_parser.js
+++ js/js_parser.js
@@ -9,7 +9,7 @@ var orig_code = fs.readFileSync("./xmi_processor.js",'utf-8');
 debugger;
 var ast = jsp.parse(orig_code); // parse code and get the initial AST
 //var final_code = pro.gen_code(ast); // compressed code here
-var xmi = xmi_processor.prc(ast);
+var xmi = xmi_processor.process(ast);
 debugger;
 console.log(JSON.stringify(ast),null,3);
 console.log(xmi);
--- js/xmi_processor.js
+++ js/xmi_processor.js
@@ -1,136 +1,155 @@
 var pro = require("uglify-js").uglify;
 
 function XMI_PROCESSOR() {
-	var bla = "BLUB";
-	this.blubb = 'fasel';
+	this.stack = [];
 };
 
-XMI_PROCESSOR.prototype.prc = function prc(node) {
+XMI_PROCESSOR.prototype.process = function process(root) {
+	this.stack = [];
+	return this.prc(this.stack, root);
+};
+
+XMI_PROCESSOR.prototype.prc = function prc(stack, node) {
 	var code = "";
+	debugger;
 	try {
 		var op = node[0];
-		code = this['_' + op].apply(this, node.slice(1));
+		var args = node.slice(1);
+		args.unshift(stack);
+		debugger;
+		this['_' + op].apply(this, args);
+		code = stack.join('');
 	} catch (ex) {
 		code = '<<' + op + '>> ' + ex;
 	}
 	return code;
 };
 
-XMI_PROCESSOR.prototype._toplevel = function _toplevel(arglist) {
-	var code = '<TOPLEVEL>\n';
+XMI_PROCESSOR.prototype._toplevel = function _toplevel(stack, arglist) {
 	var me = this;
-	arglist.forEach(function cb_each_arg(arg, i) {
-		code += me.prc(arg);
-	});
-	return code + "\n</TOPLEVEL>\n";
+	function get_cb(stack) {
+		return function cb_each_arg(arg, i) {
+			me.prc(stack, arg);
+		};
+	}
+	stack.push('<TOPLEVEL>\n');
+	debugger;
+	arglist.forEach(get_cb(stack));
+	stack.push("</TOPLEVEL>\n");
 };
 
-XMI_PROCESSOR.prototype._stat = function _stat(arglist) {
-	var code = '<STATEMENT>\n';
-	code += this.prc(arglist);
-	return code + "\n</STATEMENT>\n";
+XMI_PROCESSOR.prototype._stat = function _stat(stack, arglist) {
+	stack.push('\n<STATEMENT>\n');
+	this.prc(stack, arglist);
+	stack.push("</STATEMENT>\n");
 };
 
-XMI_PROCESSOR.prototype._object = function _object(decl) {
-	var code = '<OBJECT>\n';
+XMI_PROCESSOR.prototype._object = function _object(stack, decl) {
 	var me = this;
-	decl.forEach(function cb_each_decl(prop, i) {
-		code += '<PROPERTY>' + prop[0] + ' : ' + me.prc(prop[1])
-				+ '</PROPERTY>\n';
-	});
-	return code + "\n</OBJECT>\n";
+	function get_cb(stack) {
+		return function cb_each_decl(prop, i) {
+			code += '<PROPERTY>' + prop[0] + ' : ' + me.prc(stack, prop[1])
+					+ '</PROPERTY>\n';
+		};
+	}
+	stack.push('<OBJECT>\n');
+	decl.forEach(get_cb(stack));
+	stack.push("\n</OBJECT>\n");
 };
 
-XMI_PROCESSOR.prototype._dot = function _dot(lvalue, rvalue, multi) {
-	var code = multi ? '' : '<IDENT>';
-debugger;	
-	if (this._is_leaf(lvalue)){
-		code += lvalue;
+XMI_PROCESSOR.prototype._dot = function _dot(stack, lvalue, rvalue, multi) {
+	stack.push(multi ? '' : '<IDENT>');
+	debugger;
+	if (this._is_leaf(lvalue)) {
+		stack.push(lvalue);
 	} else {
-		if (lvalue[0] == 'dot'){
+		if (lvalue[0] == 'dot') {
 			var args = lvalue.slice(1);
 			var lval = args[0];
 			var rval = args[1];
-			code += this._dot(lval,rval,true);
+			this._dot(stack, lval, rval, true);
 		} else {
-			code += this.prc(lvalue);
+			this.prc(stack, lvalue);
 		}
 	}
 
-	
-	if (this._is_leaf(rvalue)){
-		code += '.'+rvalue;
+	if (this._is_leaf(rvalue)) {
+		stack.push('.');
+		stack.push(rvalue);
 	} else {
-		if (rvalue[0] == 'dot'){
+		if (rvalue[0] == 'dot') {
 			args = rvalue.slice(1);
 			lval = args[0];
 			rval = args[1];
-			code += this._dot(lval,rval,true);
+			this._dot(stack, lval, rval, true);
 		} else {
-			code += this.prc(rvalue);
+			this.prc(stack, rvalue);
 		}
 	}
-	return code + (multi ? '' : "</IDENT>\n");
+	stack.push(multi ? '' : "</IDENT>\n");
 };
 
 XMI_PROCESSOR.prototype._is_leaf = function _is_leaf(node) {
 	return node.constructor != Array;
 };
 
-XMI_PROCESSOR.prototype._assign = function _assign(unused, lvalue, rvalue) {
-	var code = '<ASSIGN>\n';
-	code += this.prc(lvalue) + ' = ' + this.prc(rvalue);
-	return code + "\n</ASSIGN>\n";
+XMI_PROCESSOR.prototype._assign = function _assign(stack, unused, lvalue,
+		rvalue) {
+	stack.push('<ASSIGN>\n');
+	this.prc(stack, lvalue);
+	stack.push(' = ');
+	this.prc(stack, rvalue);
+	stack.push("</ASSIGN>\n");
 };
 
-XMI_PROCESSOR.prototype._defun = function _defun(name, args, body) {
-	var code = '<DEFUN>\n<NAME>';
-	code += name + '</NAME>\n<ARGS>' + args + '</ARGS>\n<BODY>\n';
-	var ast = ['toplevel'];
+XMI_PROCESSOR.prototype._defun = function _defun(stack, name, args, body) {
+	stack.push('<DEFUN>\n<NAME>');
+	stack.push(name + '</NAME>\n<ARGS>' + args + '</ARGS>\n<BODY>\n');
+	var ast = [ 'toplevel' ];
 	ast.push(body);
-	code += pro.gen_code(ast,{beautify: false});
-	code += '\n</BODY>\n';
-	return code + "\n</DEFUN>\n";
+	stack.push(pro.gen_code(ast, {
+		beautify : false
+	}));
+	stack.push('\n</BODY>\n');
+	stack.push("</DEFUN>\n");
 };
 
-XMI_PROCESSOR.prototype._string = function _string(elem) {
-	return elem;
+XMI_PROCESSOR.prototype._string = function _string(stack, elem) {
+	stack.push(elem);
 };
 
-XMI_PROCESSOR.prototype._name = function _name(elem) {
-	return elem;
+XMI_PROCESSOR.prototype._name = function _name(stack, elem) {
+	stack.push(elem);
 };
 
-XMI_PROCESSOR.prototype._function = function _function(name, args, body) {
+XMI_PROCESSOR.prototype._function = function _function(stack, name, args, body) {
 	debugger;
-	var	code = '<FUNCTIION>' + this._defun(name,args,body) + '</FUNCTIION>\n';
-	return code;
+	stack.push('<FUNCTIION>');
+	this._defun(stack, name, args, body);
+	stack.push('</FUNCTIION>\n');
 };
 
-XMI_PROCESSOR.prototype._block = function _block(elem) {
-	var code = '';
+XMI_PROCESSOR.prototype._block = function _block(stack, elem) {
 	if (elem) {
-		code = '<BLOCK>' + elem + '</BLOCK>\n';
+		stack.push('<BLOCK>' + elem + '</BLOCK>\n');
 	}
-	return code;
 };
 
-XMI_PROCESSOR.prototype._var = function _var(statement) {
+XMI_PROCESSOR.prototype._var = function _var(stack, statement) {
 	var code = '';
 	var decl = statement ? statement[0] : null;
 	if (decl) {
-		code = '<VAR>' + decl[0] + '<DECL>'+ this.prc(decl[1]) + '</DECL>\n</VAR>\n';
+		stack.push('<VAR>' + decl[0] + '<DECL>');
+		this.prc(stack, decl[1]);
+		stack.push('</DECL>\n</VAR>\n');
 	}
-	return code;
 };
 
-XMI_PROCESSOR.prototype._call = function _call(fun, args) {
-	var code = '';
+XMI_PROCESSOR.prototype._call = function _call(stack, fun, args) {
 	debugger;
 	if (fun) {
-		code = '<CALL>' + JSON.stringify([fun,args]) + '</CALL>\n';
+		stack.push('<CALL>' + JSON.stringify([ fun, args ]) + '</CALL>\n');
 	}
-	return code;
 };
 
 // #######################################################
